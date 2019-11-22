426fc5d8e067d9b3830c27f99f46a9728f3132b7
pavan <dongare.pavan25@gmail.com>
2019-11-23 05:04:32 +0530

    new angular code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
