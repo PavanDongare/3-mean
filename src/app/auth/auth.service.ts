4e17551911a042be8f41b408bca43b5871192305
pavan <dongare.pavan25@gmail.com>
2020-03-28 13:34:41 +0530

    take to main page on login , signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AuthData } from './auth.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -7,13 +8,14 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private router : Router ,private http: HttpClient) { }
 
 createUser(email: string, password: string ) {
       const authData : AuthData = { email :email, password:password};
       this.http.post("http://localhost:3000/api/user/signup",authData)
       .subscribe(response => {
         console.log(response);
+        this.router.navigate(['/']);
       });
   }
 
@@ -22,6 +24,7 @@ createUser(email: string, password: string ) {
     this.http.post("http://localhost:3000/api/user/login",authData)
       .subscribe(result=>{
         console.log(result);
+        this.router.navigate(['/']);
       });
   }
 }
