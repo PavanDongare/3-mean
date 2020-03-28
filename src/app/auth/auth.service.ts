dd1d4d47858efd4f78a207ec3266794f5621ea13
pavan <dongare.pavan25@gmail.com>
2020-03-28 16:30:14 +0530

    auth status subject

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,23 +2,34 @@ import { Router } from '@angular/router';
 import { AuthData } from './auth.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  token: string;
+  private token: string;
 
+  private authStatus = new Subject<boolean>();
   getToken() {
     return this.token;
   }
 
+  getAuthStatus(){
+    return this.authStatus.asObservable();
+  }
+
   constructor(private router : Router ,private http: HttpClient) { }
 
+
+
+
+
+
 createUser(email: string, password: string ) {
-      const authData : AuthData = { email :email, password:password};
-      this.http.post("http://localhost:3000/api/user/signup",authData)
+      const authData: AuthData = { email, password};
+      this.http.post('http://localhost:3000/api/user/signup', authData)
       .subscribe(response => {
         console.log(response);
         this.router.navigate(['/']);
@@ -26,12 +37,13 @@ createUser(email: string, password: string ) {
   }
 
   login(email: string , password: string) {
-    const authData : AuthData = { email:email, password:password};
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login",authData)
-      .subscribe(response =>{
+    const authData: AuthData = { email , password } ;
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+      .subscribe(response => {
         console.log(response);
         const token = response.token;
         this.token = token;
+        this.authStatus.next(true);
         this.router.navigate(['/']);
       });
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  private authListener : Subscription;
 
   ngOnInit() {
+    this.authListener =  this.authService.getAuthStatus()
+        .subscribe( isAuthenticated =>  {
+          this.isAuthenticated = isAuthenticated;
+        });
   }
 
+  ngOnDestroy() {
+    this.authListener.unsubscribe();
+  }
 }
