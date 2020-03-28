776c690d3ff7e67c8cd551eb182e2c6e88133e6e
pavan <dongare.pavan25@gmail.com>
2020-03-28 13:30:55 +0530

    access restricted

--- backend/middleware/check.auth.js
+++ backend/middleware/check.auth.js
@@ -5,10 +5,10 @@ const jwt = require("jsonwebtoken");
 module.exports = (req,res,next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token,"");
-    next(); 
+    jwt.verify(token,"secret_this_should_be_longer");
+    next();
   }
-  catch {
+  catch(error) {
   res.status(200).json({message:"auth fail"});
   }
 }
